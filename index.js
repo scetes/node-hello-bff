7373ca2d1f1a8ac1b126a77979bff1376c207ee1
scetes <scetermacek@gmail.com>
2019-11-16 20:19:59 -0500

    xray

--- index.js
+++ index.js
@@ -2,13 +2,38 @@ var express = require("express"),
     path = require("path");
 
 
-const request = require('request');
-
+//const request = require('request');
+//var AWSXRay = require('aws-xray-sdk');
 
 app = express();
 
+//app.use(AWSXRay.express.openSegment('MyApp'));
+
+function getHealth(callback){
+    request('http://node-hello-alb-1752502560.us-east-1.elb.amazonaws.com:8088/api/9', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            result = '{"Greeting", "Hello"};          
+            return callback(null, result);
+        } else {            
+            return callback(error, null);;
+        }
+    });
+}
+
+
 function getRoutes(callback){
     request('http://node-hello-alb-1752502560.us-east-1.elb.amazonaws.com:8088/api/9', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            result = JSON.stringify(JSON.parse(body));          
+            return callback(null, result);
+        } else {            
+            return callback(error, null);;
+        }
+    });
+}
+
+function getRoutesMesh(callback){
+    request('http://:8088/api/9', function(error, response, body) {
         if (!error && response.statusCode == 200) {
             result = JSON.stringify(JSON.parse(body));          
             return callback(null, result);
@@ -27,6 +52,24 @@ app.get('/factor9', function(req, res) {
 
 });
 
+app.get('/', function(req, res) {
+
+    getHealth(function(err, data){ 
+        if(err) return res.send(err);       
+        res.send('Response from Factor function: ' + data);
+    });
+
+});
+
+
+app.get('/mesh', function(req, res) {
+
+    getRoutes(function(err, data){ 
+        if(err) return res.send(err);       
+        res.send('Response from Factor function: ' + data);
+    });
+
+});
 
 
 
