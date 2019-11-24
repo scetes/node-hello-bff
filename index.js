88e37e3b2255ebed5af9bd4338f204dea5c7d698
scetes <scetermacek@gmail.com>
2019-11-23 23:17:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function getRoutes(callback){
 }
 
 function getRoutesMesh(callback){
-    request('http://:8088/api/9', function(error, response, body) {
+    request('http://node-hello-fargate-appmesh-service.local:8088/api/9', function(error, response, body) {
         if (!error && response.statusCode == 200) {
             result = JSON.stringify(JSON.parse(body));          
             return callback(null, result);
